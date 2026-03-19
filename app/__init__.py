f353f22684c741c42b797b79ba6a2707bfdbf0c6
Glauco Vasconcelos <113292955+Andrade-14@users.noreply.github.com>
2026-03-19 18:57:45 +0000

    projeto em fase inicial, ainda nao esta funcionando completamente

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from .routes.messages import messages_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(messages_bp, url_prefix="/messages")
+
+    return app
