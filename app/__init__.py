37e6118c2e363922fdff5a713ac37977bbbc7ea8
Glauco Vasconcelos de Andrade <andrade.v@academico.ifrn.edu.br>
2026-03-26 14:29:42 -0300

    cap4

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,29 @@
 from flask import Flask
-from .routes.messages import messages_bp
 from .config import Config
+from .extensions import db, migrate, ma
+from .routes.messages import messages_bp
+from marshmallow import ValidationError
+from werkzeug.exceptions import NotFound
 
 def create_app():
     app = Flask(__name__)
+
     app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    ma.init_app(app)
+
+    from .models import message
+
     app.register_blueprint(messages_bp, url_prefix="/messages")
+
+    @app.errorhandler(ValidationError)
+    def handle_validation_error(err):
+        return {"errors": err.messages}, 400
+
+    @app.errorhandler(NotFound)
+    def handle_not_found(err):
+        return {"erro": "Recurso não encontrado"}, 404
 
     return app
---
+++ app/models/message.py
@@ -0,0 +1,9 @@
+from app.extensions import db
+from datetime import datetime
+
+class Message(db.Model):
+    __tablename__ = "messages"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
